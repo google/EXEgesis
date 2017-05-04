881873b5661e74232110d1cae4dbfe59d8ce4d5d
Clement Courbet <courbet@google.com>
2017-05-04 17:27:34 +0200

    Add proto utils.

---
+++ net/proto2/util/public/repeated_field_util.h
@@ -0,0 +1,54 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NET_PROTO2_UTIL_PUBLIC_REPEATED_FIELD_UTIL_H_
+#define NET_PROTO2_UTIL_PUBLIC_REPEATED_FIELD_UTIL_H_
+
+#include <utility>
+
+#include "src/google/protobuf/repeated_field.h"
+
+namespace cpu_instructions {
+namespace google {
+namespace protobuf {
+namespace util {
+
+template <typename T>
+void Truncate(::google::protobuf::RepeatedPtrField<T>* array, int new_size) {
+  const int size = array->size();
+  CHECK_GE(size, new_size);
+  array->DeleteSubrange(new_size, size - new_size);
+}
+
+template <typename T, typename Pred>
+int RemoveIf(::google::protobuf::RepeatedPtrField<T>* array, const Pred& pr) {
+  T** const begin = array->mutable_data();
+  T** const end = begin + array->size();
+  T** write = begin;
+  while (write < end && !pr(*write)) ++write;
+  if (write == end) return 0;
+  // 'write' is positioned at first element to be removed.
+  for (T** scan = write + 1; scan < end; ++scan) {
+    if (!pr(*scan)) std::swap(*scan, *write++);
+  }
+  Truncate(array, write - begin);
+  return end - write;
+}
+
+}  // namespace util
+}  // namespace protobuf
+}  // namespace google
+}  // namespace cpu_instructions
+
+#endif  // NET_PROTO2_UTIL_PUBLIC_REPEATED_FIELD_UTIL_H_
